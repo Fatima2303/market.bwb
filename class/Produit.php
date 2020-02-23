cdf96067897fccd4661ba958c7b8db2d338fae4e
Fatima2303 <lazaouifatimapro@gmail.com>
2020-02-23 10:12:07 +0100

    Getters and Setters for class Produit Done

---
+++ class/Produit.php
@@ -0,0 +1,121 @@
+<?php
+
+class Produit
+{
+    private $id;
+    private $nomProduit;
+    private $descriptionProduit;
+    private $quantityProduit;
+    private $isAvailable;
+    private $prix;
+
+    /*********CONSTRUCT*********/
+    /*********GETTERS*********/
+    /*
+    *return $id
+    *type : integer
+    */
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /*
+    *return $nomProduit
+    * type string
+    */
+
+    public function getNomProduit()
+    {
+        return $this->nomProduit;
+    }
+
+    /*
+    *return $descriptionProduit
+    *type string
+    */
+
+    public function getDescriptionProduit()
+    {
+        return $this->descriptionProduit;
+    }
+
+    /*
+    * return $quantityProduit
+    * type integer
+    */
+    public function getQuantityProduit()
+    {
+        return $this->quantityProduit;
+    }
+    /*
+    * return $isAvailable
+    * type boolean
+    */
+    public function getIsAvailable()
+    {
+        return $this->isAvailable;
+    }
+    /*
+    *return $prix
+    *type float
+    */
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    /*********SETTERS*********/
+
+    /*
+    * set value $id
+    * type : int
+    */
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+    /*
+    *set value $nomProduit
+    * type : string
+    */
+    public function setNomProduit(string $nomProduit)
+    {
+        $this->nomProduit = $nomProduit;
+    }
+    /*
+    *set value $descriptionProduit
+    *type : string
+    */
+    public function setDescriptionProduit(string $descriptionProduit)
+    {
+        $this->descriptionProduit = $descriptionProduit;
+    }
+
+    /*
+    * set value $quantityProduit 
+    * type : integer
+    */
+    public function setQuantityProduit(int $quantityProduit)
+    {
+        $this->quantityProduit = $quantityProduit;
+    }
+
+    /*
+    *set value $isAvailable
+    * type : float
+    */
+    public function setIsAvailable(float $isAvailable)
+    {
+        $this->isAvailable = $isAvailable;
+    }
+    /*
+    *set value $prix
+    * type : float
+    */
+    public function setPrix(float $prix)
+    {
+        $this->prix = $prix;
+    }
+}
