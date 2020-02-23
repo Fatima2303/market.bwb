c0d2e8809c6bb9660f8fe8d4f6b54259f79d4cfa
Fatima2303 <lazaouifatimapro@gmail.com>
2020-02-23 09:58:51 +0100

    Getters and Setters for class Client Done

---
+++ class/Client.php
@@ -0,0 +1,135 @@
+<?php
+
+class Client
+{
+    private $id;
+    private $nom;
+    private $numeroClient;
+    private $adresse;
+    private $telephone;
+    private $mail;
+
+
+    /*********CONSTRUCT*********/
+    /*********GETTERS*********/
+
+    /**
+     * return $id
+     * type : integer
+     */
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * return $nom
+     * type : string
+     */
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * return $numeroClient
+     * type :integer
+     */
+
+    public function getNumeroClient()
+    {
+        return $this->numeroClient;
+    }
+
+    /**
+     * return $adresse
+     * type : string
+     */
+
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * return $telephone
+     * type: integer
+     */
+
+    public function getTelephone()
+    {
+        return $this->telephone;
+    }
+
+    /**
+     * return $mail
+     * type : string
+     */
+
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /*********SETTERS*********/
+
+    /**
+     * set value $id
+     * type : integer
+     */
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * set value $nom
+     * type : string
+     */
+
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * set value $numeroClient
+     * type : integer
+     */
+    public function setNumeroClient($numeroClient)
+    {
+        $this->numeroClient = $numeroClient;
+    }
+
+    /**
+     * set value $adresse
+     * type : string
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+    }
+
+    /**
+     * set value $telephone
+     * type : integer
+     */
+    public function setTelephone($telephone)
+    {
+        $this->telephone = $telephone;
+    }
+
+    /**
+     * set value $mail
+     * type : string
+     */
+
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+    }
+    /*********SPECIFICS METHODS*********/
+}
